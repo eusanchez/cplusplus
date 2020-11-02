158682ee958e6101604ab6e5dd6104723138ec07
Ana San <67754798+eusanchez@users.noreply.github.com>
2020-11-02 15:54:03 -0600

    Add files via upload
    Lectura o entrada de datos como se le llamaria en python el famoso "input"

---
+++ lecturaoentradadedatos.cpp
@@ -0,0 +1,28 @@
+//lecutra o entrada de datos
+
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int numero; //definiendo mi variable numero
+    float decimal;
+
+    cout<< "Digite un numero: ";
+    cin>>numero; //guardando mi variable numero
+
+    cout<< "Digite un numero con decimales: ";
+    cin>>decimal;
+
+    cout<<"\nEl numero que digitaste fue: "<<numero <<endl; 
+    cout<<"\nEl numero que digitaste fue: "<<decimal <<endl; 
+
+
+    return 0;
+}
+
+/* esto es un comentario de varias lineas 
+
+mi nombre es Juanita */
