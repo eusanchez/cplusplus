90d1f779c52d4b8f6cb6bdd5d905baafd51735a0
Ana San <67754798+eusanchez@users.noreply.github.com>
2020-11-02 13:32:29 -0600

    Add files via upload

---
+++ miholamundo.cpp
@@ -0,0 +1,27 @@
+// primer programa de c++
+
+#include<iostream> //iosstream es entrada y salida de datos incluye la libreria estandar
+
+using namespace std;  
+
+//si no pongo esto voy a tener que estar poniendo a acada rato doble dos 
+//puntitos para que entienda el programa que debe mostrar mi mensaje en la pantalla este lo hace mas general
+
+
+int main(){
+
+
+    cout << "Hola Mundo!" <<endl;
+
+    // lo que escriba adentro del cout me lo va a imprimir en la consola
+
+    return 0;
+}
+
+// int main es la funcion principal 
+
+//para imprimir la funcion se corre el comando g++ nombredelarchivo.cpp revisar bien el location 
+// luego corro el archivo de salida por ejemplo ./a.out 
+// puedo guardarlo con distinto nombre poniendo -o nombredemiarhicvosalida
+
+// endl para hacer salto de linea
